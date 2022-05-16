466942db946715578ed36c015bc6603eb21d52cb
Jeff <jeff.c.carmichael@gmail.com>
2022-05-15 22:25:19 -0400

    added leaflet

--- src/components/IPMAP.js
+++ src/components/IPMAP.js
@@ -1,8 +1,23 @@
+import { MapContainer, TileLayer, Marker,Popup ,useMap } from 'react-leaflet'
 
 const IpMap = () => {
+  const position = [51.505, -0.09]
   return (
-    <div className="map-main">IpMap</div>
-  )
+
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} id="map">
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+
+  
+    )
 }
 
 export default IpMap
